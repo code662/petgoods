c52f097763b490cf9ea4e54ecbd888c424157e73
jihyeong1 <130946454+jihyeong1@users.noreply.github.com>
2023-06-02 17:36:38 +0900

    [정석현] Id 작성

---
+++ src/main/java/vo/Id.java
@@ -0,0 +1,41 @@
+package vo;
+
+public class Id {
+	private int idNo;
+	private String id;
+	private String lastPw;
+	private String active;
+	private String createdate;
+	
+	public int getIdNo() {
+		return idNo;
+	}
+	public void setIdNo(int idNo) {
+		this.idNo = idNo;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getLastPw() {
+		return lastPw;
+	}
+	public void setLastPw(String lastPw) {
+		this.lastPw = lastPw;
+	}
+	public String getActive() {
+		return active;
+	}
+	public void setActive(String active) {
+		this.active = active;
+	}
+	public String getCreatedate() {
+		return createdate;
+	}
+	public void setCreatedate(String createdate) {
+		this.createdate = createdate;
+	}
+
+}
