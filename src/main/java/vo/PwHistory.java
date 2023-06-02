d8b3fffde631b48edaf518c1c57ad80918918889
jihyeong1 <130946454+jihyeong1@users.noreply.github.com>
2023-06-02 17:37:08 +0900

    [정석현] PwHistory 작성

---
+++ src/main/java/vo/PwHistory.java
@@ -0,0 +1,33 @@
+package vo;
+
+public class PwHistory {
+	private int pwNo;
+	private String id;
+	private String pw;
+	private String createdate;
+	
+	public int getPwNo() {
+		return pwNo;
+	}
+	public void setPwNo(int pwNo) {
+		this.pwNo = pwNo;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getCreatedate() {
+		return createdate;
+	}
+	public void setCreatedate(String createdate) {
+		this.createdate = createdate;
+	}
+}
