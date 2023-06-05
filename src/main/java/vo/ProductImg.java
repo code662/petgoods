24d52ce4790ac7a8710c975fb26900599018ab65
eunhye <eu99ne@gmail.com>
2023-06-05 17:46:38 +0900

    [정지형] ProductImg.java vo 작성

---
+++ src/main/java/vo/ProductImg.java
@@ -0,0 +1,53 @@
+package vo;
+
+public class ProductImg {
+	private int productImgNo;
+	private int productNo;
+	private String productOriFilename;
+	private String productSaveFilename;
+	private String productFiletype;
+	private String createdate;
+	private String updatedate;
+	public int getProductImgNo() {
+		return productImgNo;
+	}
+	public void setProductImgNo(int productImgNo) {
+		this.productImgNo = productImgNo;
+	}
+	public int getProductNo() {
+		return productNo;
+	}
+	public void setProductNo(int productNo) {
+		this.productNo = productNo;
+	}
+	public String getProductOriFilename() {
+		return productOriFilename;
+	}
+	public void setProductOriFilename(String productOriFilename) {
+		this.productOriFilename = productOriFilename;
+	}
+	public String getProductSaveFilename() {
+		return productSaveFilename;
+	}
+	public void setProductSaveFilename(String productSaveFilename) {
+		this.productSaveFilename = productSaveFilename;
+	}
+	public String getProductFiletype() {
+		return productFiletype;
+	}
+	public void setProductFiletype(String productFiletype) {
+		this.productFiletype = productFiletype;
+	}
+	public String getCreatedate() {
+		return createdate;
+	}
+	public void setCreatedate(String createdate) {
+		this.createdate = createdate;
+	}
+	public String getUpdatedate() {
+		return updatedate;
+	}
+	public void setUpdatedate(String updatedate) {
+		this.updatedate = updatedate;
+	}
+}
