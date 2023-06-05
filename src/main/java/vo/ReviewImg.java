61731582def6ccce6bbc4caa9e096270b46b5cd6
eunhye <eu99ne@gmail.com>
2023-06-05 17:47:32 +0900

    [구은혜, 정석현] ReviewImg.java vo 작성

---
+++ src/main/java/vo/ReviewImg.java
@@ -0,0 +1,54 @@
+package vo;
+
+public class ReviewImg {
+	private int reviewImgNo;
+	private int reviewNo;
+	private String reviewOriFilename;
+	private String reviewSaveFilename;
+	private String reviewFiletype;
+	private String createdate;
+	private String updatedate;
+	
+	public int getReviewImgNo() {
+		return reviewImgNo;
+	}
+	public void setReviewImgNo(int reviewImgNo) {
+		this.reviewImgNo = reviewImgNo;
+	}
+	public int getReviewNo() {
+		return reviewNo;
+	}
+	public void setReviewNo(int reviewNo) {
+		this.reviewNo = reviewNo;
+	}
+	public String getReviewOriFilename() {
+		return reviewOriFilename;
+	}
+	public void setReviewOriFilename(String reviewOriFilename) {
+		this.reviewOriFilename = reviewOriFilename;
+	}
+	public String getReviewSaveFilename() {
+		return reviewSaveFilename;
+	}
+	public void setReviewSaveFilename(String reviewSaveFilename) {
+		this.reviewSaveFilename = reviewSaveFilename;
+	}
+	public String getReviewFiletype() {
+		return reviewFiletype;
+	}
+	public void setReviewFiletype(String reviewFiletype) {
+		this.reviewFiletype = reviewFiletype;
+	}
+	public String getCreatedate() {
+		return createdate;
+	}
+	public void setCreatedate(String createdate) {
+		this.createdate = createdate;
+	}
+	public String getUpdatedate() {
+		return updatedate;
+	}
+	public void setUpdatedate(String updatedate) {
+		this.updatedate = updatedate;
+	}
+}
