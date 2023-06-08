1f8ebd6400225c6413ac24442e3cd8d42db62cad
conio11 <conio416@naver.com>
2023-06-08 17:49:41 +0900

    [오다빈] CategoryDao.java 메인 카테고리 출력 메소드 수정, 카테고리 상세정보 메소드 추가

--- src/main/java/dao/CategoryDao.java
+++ src/main/java/dao/CategoryDao.java
@@ -6,30 +6,53 @@ import util.*;
 import vo.*;
 
 public class CategoryDao {
-	// 메인 카테고리 조회
+	/*
+	 * // 메인 카테고리 조회 public ArrayList<Category> selectMainCategory() throws
+	 * Exception { // 반환할 ArrayList<Address> 생성 ArrayList<Category> list = new
+	 * ArrayList<>(); // DB 접속 DBUtil dbUtil = new DBUtil(); Connection conn =
+	 * dbUtil.getConnection(); // sql 전송 후 결과셋 반환받아 리스트에 저장 String sql =
+	 * "SELECT category_no categoryNo, category_main_name categoryMainName, category_sub_name categorySubName, createdate, updatedate FROM category"
+	 * ; PreparedStatement stmt = conn.prepareStatement(sql);
+	 * 
+	 * ResultSet rs = stmt.executeQuery(); while(rs.next()) { Category category =
+	 * new Category(); category.setCategoryNo(rs.getInt("categoryNo"));
+	 * category.setCategoryMainName(rs.getString("categoryMainName"));
+	 * category.setCategorySubName(rs.getString("categorySubName"));
+	 * category.setCreatedate(rs.getString("createdate"));
+	 * category.setUpdatedate(rs.getString("updatedate")); list.add(category); }
+	 * return list; }
+	 */
+
+	// 메인 카테고리 조회 (메인, 메인에 속한 서브 카테고리 출력)
 	public ArrayList<Category> selectMainCategory() throws Exception {
-		// 반환할 ArrayList<Address> 생성
 		ArrayList<Category> list = new ArrayList<>();
 		// DB 접속
 		DBUtil dbUtil = new DBUtil();
 		Connection conn = dbUtil.getConnection();
 		// sql 전송 후 결과셋 반환받아 리스트에 저장
-		String sql = "SELECT category_no categoryNo, category_main_name categoryMainName, category_sub_name categorySubName, createdate, updatedate FROM category";
+		// GROUP BY -> 여러 값이 그룹화되기 때문에 MIN()과 같은 집계함수 사용하지 않으면 의도와 달리 그룹 내 다른 값으로 덮어쓰기 될
+		// 수 있음
+		String sql = "SELECT MIN(category_no) categoryNo, category_main_name categoryMainName, GROUP_CONCAT(category_sub_name SEPARATOR ' || ') categorySubName, MIN(createdate) createdate, updatedate FROM category GROUP BY category_main_name ORDER BY category_no ASC";
 		PreparedStatement stmt = conn.prepareStatement(sql);
-		
+
 		ResultSet rs = stmt.executeQuery();
-		while(rs.next()) {
+		while (rs.next()) {
 			Category category = new Category();
 			category.setCategoryNo(rs.getInt("categoryNo"));
 			category.setCategoryMainName(rs.getString("categoryMainName"));
 			category.setCategorySubName(rs.getString("categorySubName"));
 			category.setCreatedate(rs.getString("createdate"));
 			category.setUpdatedate(rs.getString("updatedate"));
-			list.add(category);	
+			list.add(category);
 		}
+
 		return list;
+		/*
+		 * SELECT category_main_name, GROUP_CONCAT(category_sub_name) AS sub_categories
+		 * FROM category GROUP BY category_main_name;
+		 */
 	}
-	
+
 	// 서브 카테고리 조회
 	public ArrayList<Category> selectSubCategory(String categoryMainName) throws Exception {
 		// 반환할 ArrayList<Address> 생성
@@ -41,20 +64,20 @@ public class CategoryDao {
 		String sql = "SELECT category_no categoryNo, category_main_name categoryMainName, category_sub_name categorySubName, createdate, updatedate FROM category WHERE category_main_name = ?";
 		PreparedStatement stmt = conn.prepareStatement(sql);
 		stmt.setString(1, categoryMainName);
-		
+
 		ResultSet rs = stmt.executeQuery();
-		while(rs.next()) {
+		while (rs.next()) {
 			Category category = new Category();
 			category.setCategoryNo(rs.getInt("categoryNo"));
 			category.setCategoryMainName(rs.getString("categoryMainName"));
 			category.setCategorySubName(rs.getString("categorySubName"));
 			category.setCreatedate(rs.getString("createdate"));
 			category.setUpdatedate(rs.getString("updatedate"));
-			list.add(category);	
+			list.add(category);
 		}
 		return list;
 	}
-	
+
 	// 카테고리 추가
 	public int addCategory(Category category) throws Exception {
 		// 반환할 성공여부 변수 생성
@@ -69,10 +92,10 @@ public class CategoryDao {
 		stmt.setString(2, category.getCategorySubName());
 
 		row = stmt.executeUpdate();
-		
+
 		return row;
-	}	
-	
+	}
+
 	// 카테고리 수정
 	public int modifyCategory(Category category) throws Exception {
 		// 반환할 성공여부 변수 생성
@@ -87,23 +110,64 @@ public class CategoryDao {
 		stmt.setString(2, category.getCategorySubName());
 		stmt.setInt(3, category.getCategoryNo());
 		row = stmt.executeUpdate();
-		
+
 		return row;
-	}	
-	
+	}
+
 	// 카테고리 삭제
 	public int removeCategory(int categoryNo) throws Exception {
-		// sql 실행시 영향받은 행의 수 
+		// sql 실행시 영향받은 행의 수
 		int row = 0;
 		// db 접속
 		DBUtil dbUtil = new DBUtil();
 		Connection conn = dbUtil.getConnection();
 		// sql 전송 후 영향받은 행의 수 반환받아 저장
-		String sql ="DELETE FROM category WHERE category_no = ?";
+		String sql = "DELETE FROM category WHERE category_no = ?";
 		PreparedStatement stmt = conn.prepareStatement(sql);
 		stmt.setInt(1, categoryNo);
 		row = stmt.executeUpdate();
-		
+
 		return row;
+	}
+
+	// 카테고리 입력, 수정 시 category_main_name, category_sub_name 쌍 중복검사
+	public int checkCategorySubName(String categoryMainName, String categorySubName) throws Exception {
+		// 메인 카테고리 내 서브 카테고리명 중복 체크 변수
+		int check = 0;
+		// DB 접속
+		DBUtil dbUtil = new DBUtil();
+		Connection conn = dbUtil.getConnection();
+		// SQL 전송 후 결과셋 반환받아 리스트에 저장
+		String sql = "SELECT COUNT(*) FROM category WHERE category_main_name = ? AND category_sub_name = ?";
+		PreparedStatement stmt = conn.prepareStatement(sql);
+		stmt.setString(1, categoryMainName);
+		stmt.setString(2, categorySubName);
+		ResultSet rs = stmt.executeQuery();
+		if (rs.next()) {
+			check = rs.getInt(1);
+		}
+		return check; // 1일 경우 중복값 있음
+	}
+
+	// 카테고리 1개 상세정보
+	public Category selectCategoryOne(int categoryNo) throws Exception {
+		Category category = null;
+		// DB 연결
+		DBUtil dbUtil = new DBUtil();
+		Connection conn = dbUtil.getConnection();
+		String sql = "SELECT category_no categoryNo, category_main_name categoryMainName, category_sub_name categorySubName, createdate, updatedate FROM category WHERE category_no=?";
+		PreparedStatement stmt = conn.prepareStatement(sql);
+		stmt.setInt(1, categoryNo);
+		ResultSet rs = stmt.executeQuery();
+		category = new Category();
+		while (rs.next()) {
+			category.setCategoryNo(rs.getInt("categoryNo"));
+			category.setCategoryMainName(rs.getString("categoryMainName"));
+			category.setCategorySubName(rs.getString("categorySubName"));
+			category.setCreatedate(rs.getString("createdate"));
+			category.setUpdatedate(rs.getString("updatedate"));
+		}
+
+		return category;
 	}
 }
