54688455069de8786883cde06957db684543bb57
seokhyun03 <tjrgus0303@gmail.com>
2023-06-21 19:42:55 +0900

    [정석현] ProductDao.java 상품 조회 시 createdate 내림차순 정렬 추가, 상품 이미지 정보 조회 메서드 추가, 이미지 추가, 수정, 삭제 시 where절 조건(상품 이미지 번호에서 상품 번호로) 변경

--- src/main/java/dao/ProductDao.java
+++ src/main/java/dao/ProductDao.java
@@ -53,9 +53,9 @@ public class ProductDao {
 		 DBUtil dbUtil = new DBUtil();
 		 Connection conn = dbUtil.getConnection();
 		 //sql 전송 후 결과 셋 반환받아 리스트에 저장
-		 String sql="SELECT p.product_no productNo, p.category_no categoryNo, p.product_name productName, p.product_stock productStock, p.product_price productPrice, p.product_status productStatus, p.product_info productInfo, p.updatedate, p.createdate FROM product p INNER JOIN category c ON p.category_no = c.category_no AND c.category_main_name = ? AND c.category_sub_name = ? LIMIT ?, ?";
+		 String sql="SELECT p.product_no productNo, p.category_no categoryNo, p.product_name productName, p.product_stock productStock, p.product_price productPrice, p.product_status productStatus, p.product_info productInfo, p.updatedate, p.createdate FROM product p INNER JOIN category c ON p.category_no = c.category_no AND c.category_main_name = ? AND c.category_sub_name = ? ORDER BY createdate DESC LIMIT ?, ?";
 		 if(main.equals("전체") && sub.equals("전체")) {
-				sql="SELECT product_no productNo, category_no categoryNo, product_name productName, product_stock productStock, product_price productPrice, product_status productStatus, product_info productInfo, updatedate, createdate FROM product LIMIT ?, ?";
+				sql="SELECT product_no productNo, category_no categoryNo, product_name productName, product_stock productStock, product_price productPrice, product_status productStatus, product_info productInfo, updatedate, createdate FROM product ORDER BY createdate DESC LIMIT ?, ?";
 				PreparedStatement stmt = conn.prepareStatement(sql);
 				 stmt.setInt(1, beginRow);
 				 stmt.setInt(2, rowPerPage);
@@ -74,7 +74,7 @@ public class ProductDao {
 					 list.add(product);
 				 }
 			} else if(!main.equals("전체") && sub.equals("전체")){
-				sql="SELECT p.product_no productNo, p.category_no categoryNo, p.product_name productName, p.product_stock productStock, p.product_price productPrice, p.product_status productStatus, p.product_info productInfo, p.updatedate, p.createdate FROM product p INNER JOIN category c ON c.category_main_name = ? AND p.category_no = c.category_no LIMIT ?, ? ";
+				sql="SELECT p.product_no productNo, p.category_no categoryNo, p.product_name productName, p.product_stock productStock, p.product_price productPrice, p.product_status productStatus, p.product_info productInfo, p.updatedate, p.createdate FROM product p INNER JOIN category c ON c.category_main_name = ? AND p.category_no = c.category_no ORDER BY createdate DESC LIMIT ?, ? ";
 				PreparedStatement stmt = conn.prepareStatement(sql);
 				stmt.setString(1, main);
 				stmt.setInt(2, beginRow);
@@ -143,8 +143,7 @@ public class ProductDao {
 		 }
 		 return product;
 	 } 
-	
-	// 상품 이미지 조회
+	// 저장된 상품 이미지 이름 조회
 	public String productImgName (int productNo) throws Exception{
 		 //반환할 객체 생성
 		 String productImgName = "";
@@ -161,6 +160,32 @@ public class ProductDao {
 		 }
 		 
 		 return productImgName;
+	}
+	
+	// 상품 이미지 정보 조회
+	public ProductImg selectProductImg(int productNo) throws Exception{
+		 //반환할 객체 생성
+		ProductImg productImg = null;
+		 //db접속
+		 DBUtil dbUtil = new DBUtil();
+		 Connection conn = dbUtil.getConnection();
+		 //sql 전송 후 결과 셋 반환받아 리스트에 저장
+		 String sql="SELECT product_img_no productImgNo, product_no productNo, product_ori_filename productOriFilename, product_save_filename productSaveFilename, product_filetype productFiletype, createdate, updatedate FROM product_img WHERE product_no = ?";
+		 PreparedStatement stmt = conn.prepareStatement(sql);
+		 stmt.setInt(1, productNo);
+		 ResultSet rs = stmt.executeQuery();
+		 if(rs.next()) {
+			 productImg = new ProductImg();
+			 productImg.setProductImgNo(rs.getInt("productImgNo"));
+			 productImg.setProductNo(rs.getInt("productNo"));
+			 productImg.setProductOriFilename(rs.getString("productOriFilename"));
+			 productImg.setProductSaveFilename(rs.getString("productSaveFilename"));
+			 productImg.setProductFiletype(rs.getString("productFiletype"));
+			 productImg.setCreatedate(rs.getString("createdate"));
+			 productImg.setUpdatedate(rs.getString("updatedate"));
+		 }
+		 
+		 return productImg;
 	}
 	
 	// 상품 이미지 추가
@@ -189,28 +214,29 @@ public class ProductDao {
 		 DBUtil dbUtil = new DBUtil();
 		 Connection conn = dbUtil.getConnection();
 		 //sql 전송 후 영향받은 행의 수 반환받아 저장
-		 String sql="UPDATE product_img SET product_ori_filename = ?, product_save_filename = ?, product_filetype = ?, updatedate = NOW() WHERE product_img_no = ?";
+		 String sql="UPDATE product_img SET product_ori_filename = ?, product_save_filename = ?, product_filetype = ?, updatedate = NOW() WHERE product_no = ?";
 		 PreparedStatement stmt = conn.prepareStatement(sql);
 		 stmt.setString(1,productImg.getProductOriFilename());
 		 stmt.setString(2,productImg.getProductSaveFilename());
 		 stmt.setString(3,productImg.getProductFiletype());
-		 stmt.setInt(4,productImg.getProductImgNo());
+		 stmt.setInt(4,productImg.getProductNo());
+		 System.out.println(stmt);
 		 row = stmt.executeUpdate();
 		
 		 return row;
 	 }
 	
 	// 상품 이미지 삭제
-	public int removeProductImg(int productImgNo) throws Exception{
+	public int removeProductImg(int productNo) throws Exception{
 		 //sql 실행 시 영향받은 행의 수
 		 int row = 0;
 		 //db 접속
 		 DBUtil dbUtil = new DBUtil();
 		 Connection conn = dbUtil.getConnection();
 		 //sql 전송 후 영향받은 행의 수 반환받아 저장
-		 String sql="DELETE FROM product_img WHERE product_img_no = ?";
+		 String sql="DELETE FROM product_img WHERE product_no = ?";
 		 PreparedStatement stmt = conn.prepareStatement(sql);
-		 stmt.setInt(1, productImgNo);
+		 stmt.setInt(1, productNo);
 		 row = stmt.executeUpdate();
 		 if(row==1) {
 				System.out.println("삭제 성공");
@@ -224,12 +250,13 @@ public class ProductDao {
 	public int addProduct(Product product) throws Exception{
 		// 실행시 영향받는 행 설정
 		int row = 0;
+		int key = 0;
 		//db접속
 		DBUtil dbUtil = new DBUtil();
 		Connection conn = dbUtil.getConnection();
 		
 		String sql = "INSERT INTO product(category_no, product_name, product_stock, product_price, product_status, product_info, updatedate, createdate) VALUES(?,?,?,?,?,?,NOW(),NOW())";
-		PreparedStatement stmt = conn.prepareStatement(sql);
+		PreparedStatement stmt = conn.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
 		stmt.setInt(1,product.getCategoryNo());
 		stmt.setString(2,product.getProductName());
 		stmt.setInt(3,product.getProductStock());
@@ -237,8 +264,12 @@ public class ProductDao {
 		stmt.setString(5,product.getProductStatus());
 		stmt.setString(6,product.getProductInfo());
 		row = stmt.executeUpdate();
+		ResultSet rs = stmt.getGeneratedKeys();
+		if(rs.next()) {
+			key = rs.getInt(1);
+		}
 		
-		return row;
+		return key;
 	}
 	
 	// 상품 수정
