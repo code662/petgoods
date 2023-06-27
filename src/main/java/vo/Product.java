a3d9700a41667df85ac412d10355bc9ba1b8142f
seokhyun03 <tjrgus0303@gmail.com>
2023-06-27 14:06:24 +0900

    [정석현] 상품 필터를 위한 vo 및 dao 수정

--- src/main/java/vo/Product.java
+++ src/main/java/vo/Product.java
@@ -3,11 +3,18 @@ package vo;
 public class Product {
 	private int productNo;
 	private int categoryNo;
+	private String categoryMainname;
+	private String categorySubname;
 	private String productName;
 	private int productStock;
 	private int productPrice;
+	private int productDiscountPrice;
+	private double discountRate;
 	private String productStatus;
 	private String productInfo;
+	private String createdate;
+	private String updatedate;
+	
 	public int getProductNo() {
 		return productNo;
 	}
@@ -19,6 +26,18 @@ public class Product {
 	}
 	public void setCategoryNo(int categoryNo) {
 		this.categoryNo = categoryNo;
+	}
+	public String getCategoryMainname() {
+		return categoryMainname;
+	}
+	public void setCategoryMainname(String categoryMainname) {
+		this.categoryMainname = categoryMainname;
+	}
+	public String getCategorySubname() {
+		return categorySubname;
+	}
+	public void setCategorySubname(String categorySubname) {
+		this.categorySubname = categorySubname;
 	}
 	public String getProductName() {
 		return productName;
@@ -37,6 +56,18 @@ public class Product {
 	}
 	public void setProductPrice(int productPrice) {
 		this.productPrice = productPrice;
+	}
+	public int getProductDiscountPrice() {
+		return productDiscountPrice;
+	}
+	public void setProductDiscountPrice(int productDiscountPrice) {
+		this.productDiscountPrice = productDiscountPrice;
+	}
+	public double getDiscountRate() {
+		return discountRate;
+	}
+	public void setDiscountRate(double discountRate) {
+		this.discountRate = discountRate;
 	}
 	public String getProductStatus() {
 		return productStatus;
@@ -62,6 +93,4 @@ public class Product {
 	public void setUpdatedate(String updatedate) {
 		this.updatedate = updatedate;
 	}
-	private String createdate;
-	private String updatedate;
 }
